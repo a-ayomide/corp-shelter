fb6a2570214b60ec3446409c163a07db0e513cd4
Daniel Apantaku <apantakuayo@gmail.com>
2021-11-09 14:18:42 +0100

    initialize

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import makeStyles  from '@material-ui/styles/makeStyles'
+
+const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  title: {
+    fontSize: 14
+  }
+})
+
+const Index = () => {
+  const classes = useStyles()
+
+  return (
+    <>
+     <div>
+       
+     </div>
+    </>
+  )
+}
+
+export default Index;
