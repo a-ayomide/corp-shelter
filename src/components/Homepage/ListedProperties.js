9d58517db4112c7ccfcda0526bec9ce7452023d6
Daniel Apantaku <apantakuayo@gmail.com>
2021-11-12 13:48:41 +0100

    package.json mod

--- src/components/Homepage/ListedProperties.js
+++ src/components/Homepage/ListedProperties.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { makeStyles, Grid, Typography, Button } from "@material-ui/core";
+import { makeStyles, Grid, Typography, Button, Card } from "@material-ui/core";
 // import style from '../styles/Home.module.css'
 import Image from 'next/image'
 import HeroImg from '../../assets/heroImg.png'
@@ -36,23 +36,41 @@ const ListedProperties = () => {
         </Typography>
         <div className={classes.buttonsContainer}>
             <Grid container spacing={3}>
-                <Grid item sm={2.4}>
+                <Grid item sm={2.5}>
                     <Button variant="contained" size='large' component="button" style={{paddingRight: 50, paddingLeft: 50 }} color="secondary">Free Apartments</Button>
                 </Grid>
-                <Grid item sm={2.4}>
+                <Grid item sm={2.5}>
                     <Button variant="contained" size='large' component="button" style={{paddingRight: 50, paddingLeft: 50 }} color="secondary">Free Apartments</Button>
                 </Grid>
-                <Grid item sm={2.4}>
+                <Grid item sm={2.5}>
                     <Button variant="contained" size='large' component="button" style={{paddingRight: 50, paddingLeft: 50 }} color="secondary">Free Apartments</Button>
                 </Grid>
-                <Grid item sm={2.4}>
+                <Grid item sm={2.5}>
                     <Button variant="contained" size='large' component="button" style={{paddingRight: 50, paddingLeft: 50 }} color="secondary">Free Apartments</Button>
                 </Grid>
-                <Grid item sm={2.4} style={{display: 'flex',alignItems: 'center'}}>
+                <Grid item sm={2.5} style={{display: 'flex',alignItems: 'center'}}>
                     <Typography> Explore all &#8594;</Typography>
                 </Grid>
             </Grid>
         </div>
+        {/* <Card className={classes.root}>
+            <CardActionArea>
+                <CardMedia
+                className={classes.media}
+                image="/static/images/cards/contemplative-reptile.jpg"
+                title="Contemplative Reptile"
+                />
+                <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                    Lizard
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                    across all continents except Antarctica
+                </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card> */}
 
         {/* <Grid container>
             <Grid item sm={6}>
